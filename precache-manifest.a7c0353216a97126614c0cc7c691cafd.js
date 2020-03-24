7835337a8e03761d7382b9ab5a1aca05ce8dce79
ignaciovi <ignvaldelvira@gmail.com>
2020-03-24 19:00:29 +0000

    Footer linkedin and github changes

---
+++ precache-manifest.a7c0353216a97126614c0cc7c691cafd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70a513d7af0e2b6988e31aff314e6807",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80f4dec2e98102dc0a1a",
+    "url": "/static/css/2.bd39f990.chunk.css"
+  },
+  {
+    "revision": "563dd5f004122ac6ac8f",
+    "url": "/static/css/main.dbb2cf10.chunk.css"
+  },
+  {
+    "revision": "80f4dec2e98102dc0a1a",
+    "url": "/static/js/2.346d8a9e.chunk.js"
+  },
+  {
+    "revision": "563dd5f004122ac6ac8f",
+    "url": "/static/js/main.7ff1bb81.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b904dcdc0a6868524f4c7b580f60e912",
+    "url": "/static/media/intro-logo.b904dcdc.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9f06f809f6cce6469a03c51c026d2f4.js"
+  "/precache-manifest.a7c0353216a97126614c0cc7c691cafd.js"
 );
 
 self.addEventListener('message', (event) => {
