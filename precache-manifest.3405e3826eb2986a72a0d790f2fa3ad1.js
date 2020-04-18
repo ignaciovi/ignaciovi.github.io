4172822891540013fa20f5cc96007e096da86c74
ignaciovi <ignvaldelvira@gmail.com>
2020-04-18 13:21:23 +0100

    Change images for projects and adding wikipedia project

---
+++ precache-manifest.3405e3826eb2986a72a0d790f2fa3ad1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956fc47488515aa289d79fcb2bc57402",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80f4dec2e98102dc0a1a",
+    "url": "/static/css/2.bd39f990.chunk.css"
+  },
+  {
+    "revision": "e76bca649031cf081722",
+    "url": "/static/css/main.23d93d75.chunk.css"
+  },
+  {
+    "revision": "80f4dec2e98102dc0a1a",
+    "url": "/static/js/2.346d8a9e.chunk.js"
+  },
+  {
+    "revision": "e76bca649031cf081722",
+    "url": "/static/js/main.0b50b398.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b904dcdc0a6868524f4c7b580f60e912",
+    "url": "/static/media/intro-logo.b904dcdc.png"
+  }
+]);
